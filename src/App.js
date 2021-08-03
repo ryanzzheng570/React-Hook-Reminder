e31f10076ee46e0a70a85689feb8c7ed23155940
Ryan <ryanzzheng@gmail.com>
2021-08-03 00:43:24 -0400

    Refactored Nav Bar

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import About from './component/About';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import AddReminder from './component/AddReminder';
 import MainMenu from './component/MainMenu';
+import Navbar from './component/Navbar/Navbar';
 
 const App = () => {
     return (
       <Router>
+        <Navbar/>
         <Route path='/' exact component={MainMenu}/>
         <Route path='/reminder' component={AddReminder}/>
         <Route path='/about' component={About}/>
