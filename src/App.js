9bfe54fd043a9b17fbfd5af35f25b8076d304f0c
Ryan <ryanzzheng@gmail.com>
2021-08-30 12:35:40 -0400

    Added MuiTheme

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 // import AddReminder from './component/AddReminder';
 import MainMenu from './component/MainMenu';
 import Navbar from './component/Navigation/Navbar';
+import { MuiThemeProvider } from '@material-ui/core';
+import { theme } from './themes/theme';
 
 const App = () => {
   return (
     <React.Fragment>
       <Navbar />
-      <Router>
-        <Route path='/' exact component={MainMenu} />
-        {/* <Route path='/reminder' component={AddReminder}/> */}
-        <Route path='/about' component={About} />
-      </Router>
+      <MuiThemeProvider theme={theme}>
+        <Router>
+          <Route path='/' exact component={MainMenu} />
+          {/* <Route path='/reminder' component={AddReminder}/> */}
+          <Route path='/about' component={About} />
+        </Router>
+      </MuiThemeProvider>
     </React.Fragment>
   );
 }
--- src/component/About.js
+++ src/component/About.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Box } from '@material-ui/core'
 
 //Info
 const About = () => {
     return (
-        <div>
+        <Box>
             <h4>This website is to demonstrate and practice the understanding of React-Hook, React-Router-Dom</h4>
             <Link to="/">Go back</Link>
-        </div>
+        </Box>
     )
 }
 
