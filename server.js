dbb9c60863f8692e92c2a66a5877eff4ffe3804e
Ryan <ryanzzheng@gmail.com>
2021-09-04 18:37:52 -0400

    Added middleware
    Removed unwanted scripts
    
    
    Fixed error 
    
    
    Fixed errors

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const cors = require('cors');
-
+const path = require('path');
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// ** MIDDLEWARE ** //
+const whitelist = ['http://localhost:3000', 'http://localhost:5000', 'https://react-sushi-restaurant-online.herokuapp.com/']
+const corsOptions = {
+    origin: function (origin, callback) {
+        console.log("** Origin of request " + origin)
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            console.log("Origin acceptable")
+            callback(null, true)
+        } else {
+            console.log("Origin rejected")
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+app.use(cors(corsOptions))
 
 app.get('/api/sushi', cors(), (req, res) => {
     const sushi = [
@@ -46,10 +65,6 @@ app.get('/api/sushi', cors(), (req, res) => {
     res.json(sushi);
 });
 
-const port = 5000;
-
-app.listen(port, () => `Server running on port ${port}`);
-
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
     app.use(express.static(path.join(__dirname, 'client/build')));
@@ -58,3 +73,9 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
 }
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, (req, res) => {
+    console.log(`server listening on port: ${PORT}`)
+});
+
