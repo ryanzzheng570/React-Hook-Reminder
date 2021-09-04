d82d70a66cda0916fd19b180abbfb0f54bb4f2c9
Ryan <ryanzzheng@gmail.com>
2021-09-04 17:51:09 -0400

    Added Heroku script build and client build

--- server.js
+++ server.js
@@ -49,3 +49,12 @@ app.get('/api/sushi', cors(), (req, res) => {
 const port = 5000;
 
 app.listen(port, () => `Server running on port ${port}`);
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
